21e4033bcf5f24037a58ef1fadc3b5c9a2eb68b4
Christopher Ripley <clripley@gmail.com>
2020-07-05 00:53:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
 function happyCustomHolidayTo(holiday, name){
-  returns (`Happy ${holiday}, ${name}!`)
+  return (`Happy ${holiday}, ${name}!`)
 }
